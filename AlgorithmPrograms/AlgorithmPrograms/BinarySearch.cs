e493915dee61fa0eb30b999e2452594291172446
pappurathod <prathod4545@gmail.com>
2022-03-31 16:48:06 +0530

    Binary search

---
+++ AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public int binarySearch(String Search_ele)
+        {
+            String[] arr = { "Apple", "Banana", "Orange", "Grapes" };
+            int left_ind = 0, right_ind = arr.Length - 1;
+            while (left_ind <= right_ind)
+            {
+                int temp = left_ind + (right_ind - left_ind)/2;
+                int res = Search_ele.CompareTo(arr[temp]);
+
+                if (res == 0)
+                    return temp;
+                if (res > 0)
+                    left_ind = temp + 1;
+                else
+                    right_ind = temp - 1;
+            }
+            return 1;
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -7,6 +7,15 @@ namespace AlgorithmPrograms
         {
             StringPermutation stringPermutation = new StringPermutation();
             stringPermutation.UserInput();
+
+            BinarySearch bSearch = new BinarySearch();
+            Console.WriteLine("Enter the element for search ");
+            string word = Console.ReadLine();
+            int result = bSearch.binarySearch(word);
+            if (result == 1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at index " + result);
         }
     }
 }
