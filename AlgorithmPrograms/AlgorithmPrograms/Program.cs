47b8dc893b2f0abfe0ed0d24055aa68d54559821
pappurathod <prathod4545@gmail.com>
2022-03-31 17:16:21 +0530

    Insertion Sort

--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -5,17 +5,47 @@ namespace AlgorithmPrograms
     {
         public static void Main(String[] args)
         {
-            StringPermutation stringPermutation = new StringPermutation();
-            stringPermutation.UserInput();
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1 For String Permutations");
+            Console.WriteLine("2 For Binary Search");
+            Console.WriteLine("3 For Insertion Sort");
+            int choice = int.Parse(Console.ReadLine());
 
-            BinarySearch bSearch = new BinarySearch();
-            Console.WriteLine("Enter the element for search ");
-            string word = Console.ReadLine();
-            int result = bSearch.binarySearch(word);
-            if (result == 1)
-                Console.WriteLine("Element not present");
-            else
-                Console.WriteLine("Element found at index " + result);
+            switch (choice)
+            {
+                case 1:
+                    StringPermutation stringPermutation = new StringPermutation();
+                    stringPermutation.UserInput();
+                    break;
+                case 2:
+                    BinarySearch bSearch = new BinarySearch();
+                    Console.WriteLine("Enter the element for search ");
+                    string word = Console.ReadLine();
+                    int result = bSearch.binarySearch(word);
+                    if (result == 1)
+                        Console.WriteLine("Element not present");
+                    else
+                        Console.WriteLine("Element found at index " + result);
+                    break;
+                case 3:
+                    InsertionSort insertionSort = new InsertionSort();
+                    string GivenString = "Mango Apple Banana Watermelon Orange Grapes";
+                    string[] SplitString = GivenString.Split(" ");
+                    Console.Write("Sorted string : ");
+                    insertionSort.InsertSort(SplitString);
+                    foreach (var item in SplitString)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    break;
+
+                case 10:
+                    Console.Write("Thank you");
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+            }
         }
     }
 }
